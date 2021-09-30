34bf8081ff12f1ec8ad9f3c141bbf2f69ccbc468
Vinz <ballutvincent@gmail.com>
2021-09-30 20:51:55 +0200

    use component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { CareerComponent } from './components/career/career.component';
+import { FormationComponent } from './components/formation/formation.component';
+import { PersonalDataComponent } from './components/personal-data/personal-data.component';
+import { ShowroomComponent } from './components/showroom/showroom.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    CareerComponent,
+    FormationComponent,
+    PersonalDataComponent,
+    ShowroomComponent,
+    SkillsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
